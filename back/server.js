b5dd8cd5904fd207bc77e298298cf5112b05a501
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-25 11:57:24 +0100

    added static server

--- back/server.js
+++ back/server.js
@@ -1,1 +1,18 @@
-console.log("about to start a server...");
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+const port = 3000;
+const www = "./public";
+
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
+});
+
+app.use(express.static(www));
+app.use(serveIndex(www));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});

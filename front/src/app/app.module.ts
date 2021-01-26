87c7dbf1f544da6337cac5c27d0a2b9e9093f859
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-26 16:48:33 +0100

    pipe ok

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,18 +7,15 @@ import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { WidgetModule } from './widget/widget.module';
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LegalComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LayoutModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, LegalComponent],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, WidgetModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

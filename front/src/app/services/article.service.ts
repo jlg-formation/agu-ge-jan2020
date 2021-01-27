91bf6a675ac6f4e92e2b5c2fddac728c7d392b46
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-27 12:12:32 +0100

    added interface

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles = ['tournevis', 'pince'];
+  articles: Article[] = [
+    { name: 'Tournevis', price: 2.99, qty: 123 },
+    { name: 'Pince', price: 2, qty: 34 },
+    { name: 'Marteau', price: 2.5, qty: 2 },
+    { name: 'Scie', price: 10, qty: 1000 },
+    { name: 'Clé', price: 13.45, qty: 340 },
+  ];
   constructor() {
     console.log('article service');
   }

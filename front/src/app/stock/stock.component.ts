75c957b79de52fedd599d9aac9914e66f7a917d9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-27 15:56:45 +0100

    added suppress btn dynamic

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 import { ArticleService } from '../services/article.service';
 
 @Component({
@@ -7,7 +8,16 @@ import { ArticleService } from '../services/article.service';
   styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  selectedArticles: Article[] = [];
   constructor(public readonly articleService: ArticleService) {}
 
   ngOnInit(): void {}
+
+  toggle(a: Article): void {
+    if (this.selectedArticles.includes(a)) {
+      this.selectedArticles = this.selectedArticles.filter((art) => art !== a);
+      return;
+    }
+    this.selectedArticles.push(a);
+  }
 }

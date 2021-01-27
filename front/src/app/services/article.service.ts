620f00519619a9a3b51fbcacb33de723d586ad91
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-01-27 15:24:28 +0100

    add article ok

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -5,16 +5,29 @@ import { Article } from '../interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { name: 'Tournevis', price: 2.99, qty: 123 },
-    { name: 'Pince', price: 2, qty: 34 },
-    { name: 'Marteau', price: 2.5, qty: 2 },
-    { name: 'Scie', price: 10, qty: 1000 },
-    { name: 'Clé', price: 13.45, qty: 340 },
-  ];
+  articles: Article[] = this.getArticles();
   constructor() {}
+
+  getArticles(): Article[] {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        { name: 'Tournevis', price: 2.99, qty: 123 },
+        { name: 'Pince', price: 2, qty: 34 },
+        { name: 'Marteau', price: 2.5, qty: 2 },
+        { name: 'Scie', price: 10, qty: 1000 },
+        { name: 'Clé', price: 13.45, qty: 340 },
+      ];
+    }
+    return JSON.parse(str) as Article[];
+  }
+
+  save(): void {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
+  }
 
   add(article: Article): void {
     this.articles.push(article);
+    this.save();
   }
 }
